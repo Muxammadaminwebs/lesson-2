1ca39f8c33da64c36c6d4383b5d30f6a37410487
Muxammadaminwebs <qobiljonovmuxammadamin@gmail.com>
2022-08-12 22:33:55 +0500

    JavaScript

---
+++ JavaScript/style.js
@@ -0,0 +1,292 @@
+// ===== 1-Misol ===== //
+
+// let a = +prompt(' santi-metrni kiriting');
+// let = 1;
+// console.log(a / 100 + "metr");
+
+
+// ===== 2-Misol ===== //
+
+// let a = +prompt(' kilagramni  kiriting');
+//  let = 1;
+//  console.log(a / 1000 + "tonna");
+
+
+// ===== 3-Misol ===== //
+
+// let c = +prompt(' sonni kiriting');
+// let kesma = +prompt('ikkinchi sonni kiriting');
+//  console.log(c/ kesma +   "  b-kesma");
+
+
+// ===== 4-Misol ===== //
+
+// let son = 92;
+// let firstNumber = (Math.floor(son / 10)) * 10;
+// let secondNumber = son %= 10;
+// console.log(` ${firstNumber} ,  ${secondNumber}`);
+
+
+// ===== 5-Misol ===== //
+
+// let a = +prompt('a sonni kiriting');
+// let first = Math.floor(a / 10);
+// let second = a % 10;
+// console.log(first + second);
+
+
+// ===== 6-Misol ===== //
+
+// let a = +prompt('a sonni kiriting');
+// let firts = Math.floor(a / 10);
+// let second = a % 10;
+// alert(firts * second);
+
+
+// ===== 7-Misol ===== //
+
+// let number = +prompt('sonni kiriting');
+// let number2 = (number % 1000);
+// alert(number2);
+
+
+// ===== 8-Misol ===== //
+
+//  let seconds = 74387;
+// let hour = 3600;
+// let minut = 60;
+// let vaqt = ;
+// let hourValue = Math.floor(seconds / hour);
+// let minutValue = seconds %= minut;
+// console.log(`${hourValue}soat ${minutValue}minut `);
+ 
+
+// ======== if va else boyicha misollar ======== //
+
+
+// ===== 1-Misol ===== //
+
+// let son = +prompt('Enter son')
+// if (son > 0) {
+//      alert(true);
+// }else{
+//     alert(false)
+// }
+
+
+// ===== 2-Misol ===== //
+
+// let son = +prompt('Enter son')
+// if (son % 2 == 0) {
+//     alert('juft son ekam ustoz')
+// } else {
+//     alert('toq son ekan ustoz')
+// }
+
+
+// ===== 3-Misol ===== //
+
+// let son = +prompt('Enter son')
+// if (son % 2 == 0) {
+//     alert('juft son ekam ustoz')
+// } else {
+//     alert('toq son ekan ustoz')
+// }
+
+
+// ===== 4-Misol ===== //
+
+// let son1 = +prompt('qiymatni kiriting');
+// let son2 = +prompt(' ikkinchi qiymatni kiriting');
+// if (son1 > 0 && son2 > 0) {
+//     alert('numbers are positive');
+// } else if (son1 < 0 && son2 > 0) {
+//     alert('the first number negative .. the second number positive');
+// } else if (son1 > 0 && son2 < 0) {
+//     alert('the first number positive .. the second number negative');
+// } else {
+//     alert('numbers are negative');
+// }
+
+
+// ===== 5-Misol ===== //
+
+// let a_son = +prompt('Enter a-son');
+// let b_son = +prompt('Enter b-son');
+// let c_son = +prompt('Enter c-son');
+
+// if (a_son > b_son && b_son > c_son) {
+//     alert(true)
+// } else {
+//     alert(false)
+// }
+
+
+// ===== 6-Misol ===== //
+
+// let a_son = +prompt('Enter a-son');
+// let b_son = +prompt('Enter b-son');
+// let c_son = +prompt('Enter c-son');
+
+// if (a_son < b_son && b_son < c_son) {
+//     alert(true)
+// } else {
+//     alert(false)
+// }
+
+
+// ===== 7-Misol ===== //
+
+// let a_son = +prompt('Enter a-son');
+// let b_son = +prompt('Enter b-son');
+// if (a_son % 2 == 0 && b_son % 2 == 0) {
+//     alert(false)
+// } else {
+//     alert(true)
+// }
+
+
+// ===== 8-Misol ===== //
+
+// let a_son = +prompt('Enter a-son'),
+// b_son = +prompt('Enter b-son'),
+// c_son = +prompt('Enter c-son'),
+//     d_son = +prompt('Enter d-son');
+// if (a_son > 0 && b_son > 0 && c_son > 0 && d_son > 0) {
+//     alert(true)
+// } else {
+//     alert(false)
+// }
+
+
+// ===== 9-Misol ===== //
+
+// let a_son = +prompt('Enter a-son'),
+//     b_son = +prompt('Enter b-son'),
+//     c_son = +prompt('Enter c-son');
+// if (a_son > 0 && b_son > 0 && c_son < 0) {
+//         alert(true)
+// } else if(a_son > 0 && b_son < 0 && c_son > 0) {
+//     alert(true)
+// } else if (a_son < 0 && b_son > 0 && c_son > 0) {
+//     alert(true)
+// } else {
+//     alert(false)
+//  }
+
+
+// ===== 10-Misol ===== //
+
+// let a_son = +prompt('Son kiriting')
+// if (a_son == 1) {
+//     alert('Dushanba')
+// }
+// else if (a_son == 2) {
+//     alert('Seshanba')
+// }else if (a_son == 3) {
+//     alert('Chorshanba')
+// }else if (a_son == 4) {
+//     alert('Payshanba')
+// }else if (a_son == 5) {
+//     alert('Juma')
+// }else if (a_son == 6) {
+//     alert('Shanba')
+// }else if (a_son == 7) {
+//     alert('Yakshanba')
+// }
+
+
+// ===== 11-Misol ===== //
+
+// let a_son = +prompt('Son kiriting')
+// if (a_son == +99894) {
+//     alert('Ucell')
+// }else if (a_son == +99893) {
+//     alert('Ucell')
+// }
+// else if (a_son == +99890) {
+//     alert('Belline')
+// }else if (a_son == +99891) {
+//     alert('Belline')
+// }
+// else if (a_son == +99833) {
+//     alert('Humans')
+// }else if (a_son == +99888) {
+//     alert('Humans')
+// }
+// else if (a_son == +99899) {
+//     alert('UzMobile')
+// }else if (a_son == +99897) {
+//     alert('Mobio.uz')
+// }
+
+
+// ===== 12-Misol ===== //
+
+// let a_son = +prompt('sonni kiriting')
+// if (a_son > 0) {
+//     a_son++
+//     alert(a_son )
+// } else{
+//     a_son ++
+//     alert(a_son)
+// }
+
+ 
+// ===== 13-Misol ===== //
+
+// let a_son = +prompt('sonni kiriting')
+// if (a_son > 0) {
+//     alert( a_son+3 )
+// } else if (a_son < 0){
+//     alert(a_son+2)
+// } else {
+//     alert(false)
+// }
+
+
+// ===== 14-Misol ===== //
+
+// let a_son = +prompt('a-sonni kiriting')
+// b_son = +prompt('b_sonni kiriting')
+// if (a_son > b_son) {
+//          alert(a_son)
+// } else if (a_son < b_son) {
+//     alert(b_son)
+//      }
+
+
+// ===== 15-Misol ===== //
+
+// let a = +prompt('a sonni kiriting')
+// let b = +prompt('b sonni kiriting')
+// let c = +prompt('c sonni kiriting')
+
+// if (b > a && a > c) {
+//     alert('a son katta ' + a)
+// } else if (b > a && b > c) {
+//     alert('b son katta' + b)    
+// } else if (c > a && c > b) {
+//     alert('c soni eng  katta ' + c)
+// } else if (a == b && a == c || b == a && b == c || c == a && c == b) {
+//     alert("a va b , c soni teng " + a + ", " + b +" " +c)
+// } else {
+//     alert("I didn't  undetstand")
+// }
+// ===== 16-Misol ===== //
+
+// let a = +prompt('a sonni kiriting')
+// let b = +prompt('b sonni kiriting')
+// let c = +prompt('c sonni kiriting')
+
+// if (b > a && a > c) {
+//     alert('a son katta ' + a)
+// } else if (b < a && b < c) {
+//     alert('b son katta' + b)    
+// } else if (c < a && c < b) {
+//     alert('c soni eng  katta ' + c)
+// } else if (a == b && a == c || b == a && b == c || c == a && c == b) {
+//     alert("a va b , c soni teng " + a + ", " + b +" " +c)
+// } else {
+//     alert("I didn't  undetstand")
+// }
